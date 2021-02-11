254986fb073b295398eeab7219e5cbe4c44815f7
Samuel Ramos <samuelramos.sjr@gmail.com>
2021-02-11 09:53:56 -0300

    Create a card profile

--- app/src/main/java/com/samuelrmos/samplecompose/MainActivity.kt
+++ app/src/main/java/com/samuelrmos/samplecompose/MainActivity.kt
@@ -2,37 +2,100 @@ package com.samuelrmos.samplecompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.samuelrmos.samplecompose.ui.theme.SampleComposeTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SampleComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = LightGray
+    ) {
+        ProfileCard()
+    }
+}
+
+@Composable
+fun ProfileCard() {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
+        elevation = 8.dp
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfilePicture()
+            ProfileContent()
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture() {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.Green),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            imageResource(id = R.drawable.download),
+            contentDescription = "Profile",
+            modifier = Modifier.size(72.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileContent() {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "John Doe",
+            style = MaterialTheme.typography.h5
+        )
+        Providers(AmbientContentAlpha provides (ContentAlpha.medium)) {
+            Text(
+                text = "Active now",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SampleComposeTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
