e464b8101dbb7ebd302491e191a0a4f8be778da7
Samuel Ramos <samuelramos.sjr@gmail.com>
2021-02-22 20:45:49 -0300

    Add new list of users, create details user screen

--- app/src/main/java/com/samuelrmos/samplecompose/MainActivity.kt
+++ app/src/main/java/com/samuelrmos/samplecompose/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.samuelrmos.samplecompose
 
+import UserProfile
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Providers
@@ -15,61 +20,125 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.samuelrmos.samplecompose.model.UserProfile
-import com.samuelrmos.samplecompose.model.userProfileList
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
+import coil.transform.CircleCropTransformation
 import com.samuelrmos.samplecompose.ui.theme.SampleComposeTheme
 import com.samuelrmos.samplecompose.ui.theme.lightGreen
+import dev.chrisbanes.accompanist.coil.CoilImage
+import userProfileList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SampleComposeTheme {
-                MainScreen()
+                UserApplication()
             }
         }
     }
 }
 
 @Composable
-fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
-    Scaffold(topBar = { AppBar() }) {
+fun UserApplication(userProfiles: List<UserProfile> = userProfileList) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "users_list") {
+
+        composable("users_list") {
+            UserListScreen(userProfiles, navController)
+        }
+
+        composable(
+            "user_detail/{userId}",
+            arguments = listOf(navArgument("userId") {
+                type = NavType.IntType
+            })
+        ) { navBackStackEntry ->
+            UserProfileDetailsScreen(
+                navBackStackEntry.arguments!!.getInt("userId"),
+                navController
+            )
+        }
+    }
+}
+
+@Composable
+fun UserListScreen(userProfiles: List<UserProfile>, navController: NavHostController?) {
+    Scaffold(topBar = {
+        AppBar(
+            title = "Users List",
+            icon = Icons.Default.Home
+        ) { }
+    }) {
         Surface(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Column {
-                for (userProfile in userProfiles)
-                    ProfileCard(userProfile = userProfile)
+            LazyColumn {
+                items(userProfiles) { userProfile ->
+                    ProfileCard(userProfile = userProfile) {
+                        navController?.navigate("user_detail/${userProfile.id}")
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun AppBar() {
+fun UserProfileDetailsScreen(userId: Int, navController: NavHostController?) {
+    val userProfile = userProfileList.first { userProfile -> userId == userProfile.id }
+    Scaffold(topBar = {
+        AppBar(
+            title = "Users profile details",
+            icon = Icons.Default.ArrowBack
+        ) {
+            navController?.navigateUp()
+        }
+    }) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                ProfilePicture(userProfile.pictureUrl, userProfile.status, 240.dp)
+                ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
+            }
+        }
+    }
+}
+
+@Composable
+fun AppBar(title: String, icon: ImageVector, iconClickAction: () -> Unit) {
     TopAppBar(
         navigationIcon = {
             Icon(
-                Icons.Default.Home,
+                icon,
                 contentDescription = "TopBar",
-                modifier = Modifier.padding(horizontal = 12.dp)
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .clickable(onClick = iconClickAction)
             )
         },
-        title = { Text("Messaging Application user") }
+        title = { Text(title) }
     )
 }
 
 @Composable
-fun ProfileCard(userProfile: UserProfile) {
+fun ProfileCard(userProfile: UserProfile, clickAction: () -> Unit) {
     Card(
         modifier = Modifier
             .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth()
-            .wrapContentHeight(align = Alignment.Top),
+            .wrapContentHeight(align = Alignment.Top)
+            .clickable(onClick = { clickAction.invoke() }),
         elevation = 8.dp,
         backgroundColor = White
     ) {
@@ -78,37 +147,41 @@ fun ProfileCard(userProfile: UserProfile) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            ProfilePicture(userProfile.drawableId, userProfile.status)
-            ProfileContent(userProfile.name, userProfile.status)
+            ProfilePicture(userProfile.pictureUrl, userProfile.status, 72.dp)
+            ProfileContent(userProfile.name, userProfile.status, Alignment.Start)
         }
     }
 }
 
 @Composable
-fun ProfilePicture(drawableId: Int, onlineStatus: Boolean) {
+fun ProfilePicture(pictureUrl: String, onlineStatus: Boolean, imageSize: Dp) {
     Card(
         shape = CircleShape,
         border = BorderStroke(
             width = 2.dp,
             color = if (onlineStatus) MaterialTheme.colors.lightGreen else Color.Red
         ),
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .size(imageSize),
         elevation = 4.dp
     ) {
-        Image(
-            bitmap = imageResource(id = drawableId),
-            contentDescription = "Profile",
-            modifier = Modifier.size(72.dp)
+        CoilImage(
+            data = pictureUrl,
+            contentDescription = "image profile",
+            requestBuilder = {
+                transformations(CircleCropTransformation())
+            },
         )
     }
 }
 
 @Composable
-fun ProfileContent(userName: String, onlineStatus: Boolean) {
+fun ProfileContent(userName: String, onlineStatus: Boolean, alignment: Alignment.Horizontal) {
     Column(
         modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = alignment
     ) {
         Providers(
             AmbientContentAlpha provides (
@@ -130,10 +203,19 @@ fun ProfileContent(userName: String, onlineStatus: Boolean) {
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun UserProfileDetailsPreview() {
     SampleComposeTheme {
-        MainScreen()
+        UserProfileDetailsScreen(userId = 0, null)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserListPreview() {
+    SampleComposeTheme {
+        UserListScreen(userProfiles = userProfileList, null)
     }
 }
